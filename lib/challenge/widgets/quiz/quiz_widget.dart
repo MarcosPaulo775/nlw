82443dd54ee200bd9426399c9c065be0484a163d
MarcosPaulo775 <marcosp.desenvolvimento@gmail.com>
2021-05-02 17:47:50 -0300

    tudo

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,49 @@
+import 'package:DevQuiz/challenge/widgets/awnser/awnser_widget.dart';
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+
+  QuizWidget({Key? key, required this.question, required this.onChange})
+      : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel awnser(int index) => widget.question.answers[index];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(height: 64),
+          Text(
+            widget.question.title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(height: 24),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AwnserWidget(
+              anwser: awnser(i),
+              isSelected: indexSelected == i,
+              disabled: indexSelected != -1,
+              onTap: () {
+                indexSelected = i;
+                widget.onChange();
+                setState(() {});
+              },
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home/home_page.dart
@@ -0,0 +1,98 @@
+import 'package:DevQuiz/challenge/challenge_page.dart';
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/home_controler.dart';
+import 'package:DevQuiz/home/home_state.dart';
+import 'package:DevQuiz/home/widgets/appBar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    // controller.getUser();
+    // controller.getQuizzes();
+    controller.fetchData();
+
+    controller.stateNotifier.addListener(() async {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller.state == HomeState.sucess) {
+      return Scaffold(
+        appBar: AppBarWidget(user: controller.user!),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(
+                    label: 'Fácil',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Médio',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Difícil',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Perito',
+                  ),
+                ],
+              ),
+              SizedBox(height: 24),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: controller.quizzes!
+                      .map(
+                        (e) => QuizCardWidget(
+                          title: e.title,
+                          completed:
+                              "${e.questionAnswered}/${e.questions.length}",
+                          percent: e.questionAnswered / e.questions.length,
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ChallengePage(
+                                        questions: e.questions,
+                                      )),
+                            );
+                          },
+                        ),
+                      )
+                      .toList(),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+          ),
+        ),
+      );
+    }
+  }
+}
